9922dce7620682d22ba07d59ef4381f32653ee32
AldanaTorrejon <aldana_ro_2014@hotmail.com>
2023-12-08 10:55:38 -0300

    Cambios del componente

--- src/components/ContentRowTop.jsx
+++ src/components/ContentRowTop.jsx
@@ -1,52 +1,66 @@
-import mandalorian from '../assets/images/mandalorian.jpg'
+import React, { useState, useEffect } from 'react';
 import DataContent from './DataContent';
-import Genres from './Genres';
 
+function ContentRowTop() {
+  const [dataBoxes, setDataBoxes] = useState([]);
 
+  useEffect(() => {
+    // SimulaciÃ³n de una API, reemplaza la URL con tu endpoint real
+    const apiUrl = 'http://localhost:3000/api/';
 
-function ContentRowTop(){
+    const fetchData = async () => {
+      try {
+        const usersResponse = await fetch(apiUrl + 'users');
+        const productsResponse = await fetch(apiUrl + 'products');
 
-    const dataBoxes = [
-      {
-        title: "Movies in Data Base",
-        amount: 21,
-        icon: 'fa-film',
-        styles: ['border-left-primary' ,'text-primary']
-      },
-      {
-        title: "Total awards",
-        amount: 79,
-        icon: 'fa-award',
-        styles: [ 'border-left-success' ,'text-success']
-      },
-      {
-        title: "Actors quantity",
-        amount: 49,
-        icon: 'fa-user',
-        styles: ['border-left-warning', 'text-warning']
+        const userData = await usersResponse.json();
+        const productsData = await productsResponse.json();
+
+        const usersDataBox = {
+          title: 'Users in Data Base',
+          amount: userData.count,
+          icon: 'fa-user',
+          styles: ['border-left-primary', 'text-primary'],
+        };
+
+        const productsDataBox = {
+          title: 'Products in Data Base',
+          amount: productsData.count,
+          icon: 'fa-shopping-bag',
+          styles: ['border-left-success', 'text-success'],
+        };
+
+        const categoriesDataBox = {
+          title: 'Categories in Data Base',
+          amount: Object.keys(productsData.countByCategory).length,
+          icon: 'fa-list',
+          styles: ['border-left-warning', 'text-warning'],
+        };
+
+        setDataBoxes([usersDataBox, productsDataBox, categoriesDataBox]);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-    ]
+    };
 
+    fetchData();
+  }, []);
 
-    return(
-        <div className="container-fluid">
-        <div className="d-sm-flex align-items-center justify-content-between mb-4">
-          <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
-        </div>
-        {/* Content Row Movies*/}
-        <div className="row">
-          {/* Movies in Data Base */}
-          {
-            dataBoxes.map((dataBox, i) =>(
-              <DataContent key={i} dataBox = {dataBox}/>
-            ))
-          }
-        </div>
-        {/* End movies in Data Base */}
-        {/* Content Row Last Movie in Data Base */}
-
+  return (
+    <div className="container-fluid">
+      <div className="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
       </div>
-    )
+      {/* Content Row */}
+      <div className="row">
+        {/* Data Boxes */}
+        {dataBoxes.map((dataBox, i) => (
+          <DataContent key={i} dataBox={dataBox} />
+        ))}
+      </div>
+      {/* End Content Row */}
+    </div>
+  );
 }
 
 export default ContentRowTop;
